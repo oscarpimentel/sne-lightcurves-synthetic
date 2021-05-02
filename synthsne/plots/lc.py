fe2ba7388d6fa4f02715ca18dffef7a404a5f8b7
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-05-02 18:54:49 -0400

    mcmc needs infinite values, not very big numbers jeje

--- synthsne/plots/lc.py
+++ synthsne/plots/lc.py
@@ -32,7 +32,7 @@ def plot_synthetic_samples(lcobj_name, lcobj, lcset_name, lcset_info, method, ne
 	ax.grid(alpha=0.5)
 	title = ''
 	title += f'SNe multiband light curve & parametric model samples\n'
-	title += f'survey={survey} [{lcset_name}] - obj={lcobj_name} [{class_names[lcobj.y]}]'+'\n'
+	title += f'survey={survey}-{"".join(band_names)} [{lcset_name}] - obj={lcobj_name} [{class_names[lcobj.y]}]'+'\n'
 	title += ' - '.join([f'method={method}']+[f'{b}-error={trace_bdict[b].get_xerror()}' for b in band_names])+'\n'
 	ax.set_title(title[:-1])
 	ax.legend(loc='upper right')

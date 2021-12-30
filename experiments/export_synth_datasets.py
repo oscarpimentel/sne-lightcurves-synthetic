621bb1390f77ef84fa5d4b59472a5cd82c0ef683
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-12-30 12:13:08 -0300

    mis

--- experiments/export_synth_datasets.py
+++ experiments/export_synth_datasets.py
@@ -12,7 +12,7 @@ from fuzzytools.prints import print_big_bar
 parser = argparse.ArgumentParser(prefix_chars='--')
 parser.add_argument('--method',  type=str, default='.')
 parser.add_argument('--kf',  type=str, default='.')
-parser.add_argument('--setn',  type=str, default='training')
+parser.add_argument('--setn',  type=str, default='train')
 main_args = parser.parse_args()
 print_big_bar()
 
@@ -37,7 +37,7 @@ from synthsne import _C
 
 kfs = lcdataset.kfolds if main_args.kf=='.' else main_args.kf
 kfs = [kfs] if isinstance(kfs, str) else kfs
-setns = [str(setn) for setn in ['training', 'val']] if main_args.setn=='.' else main_args.setn
+setns = [str(setn) for setn in ['train', 'val']] if main_args.setn=='.' else main_args.setn
 setns = [setns] if isinstance(setns, str) else setns
 
 new_lcdataset = lcdataset.copy() # copy with all original lcsets
